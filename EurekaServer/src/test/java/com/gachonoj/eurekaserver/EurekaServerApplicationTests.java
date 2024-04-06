8f1cf1d0fe69c735d92ab97d2c3c89a7b5278dc5
NaMinhyeok <nmh9097@gmail.com>
2024-04-06 17:36:32 +0900

    [add] #1 Eureka Server 초기 설정

---
+++ EurekaServer/src/test/java/com/gachonoj/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachonoj.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
