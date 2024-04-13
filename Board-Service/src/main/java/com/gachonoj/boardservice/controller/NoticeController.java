d98ecaa18addca7e9b68c54ede6368fd8895b69e
chogh824 <chogh824@naver.com>
2024-04-14 03:51:26 +0900

    [feat] #12 Gateway property 수정 완료

--- Board-Service/src/main/java/com/gachonoj/boardservice/controller/NoticeController.java
+++ Board-Service/src/main/java/com/gachonoj/boardservice/controller/NoticeController.java
@@ -10,21 +10,21 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api/board/admin")
+@RequestMapping("/api/board")
 public class NoticeController {
 
     @Autowired
     private NoticeService noticeService;
 
     // 공지사항 생성 API
-    @PostMapping("/notice")
+    @PostMapping("/admin/notice")
     public ResponseEntity<CommonResponseDto<Notice>> createNotice(@RequestBody NoticeRequestDto noticeRequestDto) {
         Notice notice = noticeService.createNotice(noticeRequestDto);
         // CommonResponseDto를 사용하여 성공 메시지와 함께 Notice 객체를 반환
         return ResponseEntity.ok(CommonResponseDto.success(notice));
     }
 
-    @DeleteMapping("/notice/{noticeId}")
+    @DeleteMapping("/admin/notice/{noticeId}")
     public ResponseEntity<CommonResponseDto<Void>> deleteNotice(@PathVariable Long noticeId) {
         noticeService.deleteNotice(noticeId);
         return ResponseEntity.ok(CommonResponseDto.success(null));
