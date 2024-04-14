5e720c86bc3e53fdc67c4e52ceeb0a55c58612ec
NaMinhyeok <nmh9097@gmail.com>
2024-04-15 01:30:01 +0900

    [feat] #24 사용자 선호 언어 조회 API 구현

---
+++ Member-Service/src/main/java/com/gachonoj/memberservice/domain/dto/response/MemberLangResponseDto.java
@@ -0,0 +1,13 @@
+package com.gachonoj.memberservice.domain.dto.response;
+
+import com.gachonoj.memberservice.domain.constant.MemberLang;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberLangResponseDto {
+    private MemberLang memberLang;
+}
