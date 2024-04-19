0836115a25183e47cf7bc19590992aa33886d68c
NaMinhyeok <nmh9097@gmail.com>
2024-04-19 16:37:51 +0900

    [refactor] #36 transactional 추가

--- Board-Service/src/main/java/com/gachonoj/boardservice/service/BoardService.java
+++ Board-Service/src/main/java/com/gachonoj/boardservice/service/BoardService.java
@@ -10,6 +10,7 @@ import com.gachonoj.boardservice.repository.ReplyRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -23,35 +24,39 @@ public class BoardService {
     private final ReplyRepository replyRepository;
 
     // 공지사항 작성
+    @Transactional
     public void createNotice(NoticeRequestDto noticeRequestDto,Long memberId) {
         Notice notice = new Notice(noticeRequestDto.getNoticeTitle(), noticeRequestDto.getNoticeContents(),memberId);
         noticeRepository.save(notice);
     }
     // 공지사항 수정
+    @Transactional
     public void updateNotice(Long noticeId, NoticeRequestDto noticeRequestDto, Long memberId) {
         Notice notice = noticeRepository.findById(noticeId).orElseThrow(() -> new IllegalArgumentException("해당 공지사항이 존재하지 않습니다."));
         notice.updateNotice(noticeRequestDto.getNoticeTitle(), noticeRequestDto.getNoticeContents(),memberId);
-        noticeRepository.save(notice);
     }
     // 공지사항 삭제
+    @Transactional
     public void deleteNotice(Long noticeId) {
         Notice notice = noticeRepository.findById(noticeId).orElseThrow(() -> new IllegalArgumentException("해당 공지사항이 존재하지 않습니다."));
         noticeRepository.delete(notice);
     }
     // 문의사항 작성
+    @Transactional
     public void createInquiry(InquiryRequestDto inquiryRequestDto, Long memberId) {
         Inquiry inquiry = new Inquiry(inquiryRequestDto.getInquiryTitle(), inquiryRequestDto.getInquiryContents(), memberId);
         inquiryRepository.save(inquiry);
     }
     // 문의사항 수정
+    @Transactional
     public void updateInquiry(Long inquiryId, InquiryRequestDto inquiryRequestDto, Long memberId) {
         Inquiry inquiry = inquiryRepository.findById(inquiryId).orElseThrow(() -> new IllegalArgumentException("해당 문의사항이 존재하지 않습니다."));
         if (inquiry.getMemberId().equals(memberId)) {
             inquiry.updateInquiry(inquiryRequestDto.getInquiryTitle(), inquiryRequestDto.getInquiryContents());
-            inquiryRepository.save(inquiry);
         }
     }
     // 문의사항 삭제 회원
+    @Transactional
     public void deleteInquiryByMember(Long inquiryId,Long memberId) {
         Inquiry inquiry = inquiryRepository.findById(inquiryId).orElseThrow(() -> new IllegalArgumentException("해당 문의사항이 존재하지 않습니다."));
         if (inquiry.getMemberId().equals(memberId)) {
