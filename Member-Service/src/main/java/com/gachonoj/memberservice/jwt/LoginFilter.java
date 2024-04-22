26b10e3e1b801d1fb060c148871b57c2315a11c1
NaMinhyeok <nmh9097@gmail.com>
2024-04-23 01:23:15 +0900

    [feat] #52 CORS 설정 추가
    PNA 오류를 해결하기 위해 헤더에 "Access-Control-Allow-Private-Network", "true" 추가했습니다.

--- Member-Service/src/main/java/com/gachonoj/memberservice/jwt/LoginFilter.java
+++ Member-Service/src/main/java/com/gachonoj/memberservice/jwt/LoginFilter.java
@@ -79,6 +79,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
         response.addHeader("Authorization", "Bearer " + accessToken);
         response.addHeader("Refresh-Token", "Bearer" + refreshToken);
+        response.addHeader("Access-Control-Allow-Private-Network", "true");
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         response.getWriter().write(json);
