e91a43c39b38c40a1e5eda185e1ce955449dd59c
chogh824 <chogh824@naver.com>
2024-05-14 02:56:09 +0900

    [feat] #91 알고리즘 등록, 수정, 삭제 Controller 수정

--- Problem-Service/src/main/java/com/gachonoj/problemservice/controller/ProblemController.java
+++ Problem-Service/src/main/java/com/gachonoj/problemservice/controller/ProblemController.java
@@ -87,34 +87,16 @@ public class ProblemController {
     }
     // 알고리즘 문제 등록
     @PostMapping("/admin/register")
-    public ResponseEntity<Map<String, Object>> registerProblem(@RequestBody ProblemRequestDto problemRequestDto) {
-        // ProblemService를 호출하여 요청 DTO에 기반하여 문제 등록
-        problemService.registerProblem(problemRequestDto); // 문제 ID 저장하지 않음
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("isSuccess", true);
-        response.put("code", HttpStatus.CREATED.value()); // 201
-        response.put("time", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))); // 현재 시간
-        response.put("msg", "문제 등록 성공");
-        // API 디자인 패턴: 성공적인 생성이 생성된 엔터티의 세부 정보를 반환하지 않는 패턴
-
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    public ResponseEntity<CommonResponseDto<Void>> registerProblem(@RequestBody ProblemRequestDto problemRequestDto) {
+        problemService.registerProblem(problemRequestDto);
+        return ResponseEntity.ok(CommonResponseDto.success());
     }
 
     // 알고리즘 문제 수정
     @PutMapping("/admin/register/{problemId}")
-    public ResponseEntity<Map<String, Object>> updateProblem(@PathVariable Long problemId, @RequestBody ProblemRequestDto problemRequestDto) {
-        // ProblemService를 호출하여 요청 DTO에 기반하여 문제 수정
+    public ResponseEntity<CommonResponseDto<Void>> updateProblem(@PathVariable Long problemId, @RequestBody ProblemRequestDto problemRequestDto) {
         problemService.updateProblem(problemId, problemRequestDto);
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("isSuccess", true);
-        response.put("code", HttpStatus.OK.value()); // 200
-        response.put("time", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))); // 현재 시간
-        response.put("msg", "문제 수정 성공");
-        // 문제 수정 성공 응답에는 수정된 문제의 세부 정보를 반환하지 않음
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return ResponseEntity.ok(CommonResponseDto.success());
     }
 
     // 알고리즘 문제 삭제
