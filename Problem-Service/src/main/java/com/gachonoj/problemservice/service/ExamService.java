95f5527640650f370fa43fee9a9b329cd84a1621
chogh824 <76734412+chogh824@users.noreply.github.com>
2024-05-16 18:39:02 +0900

    [feat] #123 시험 대기화면 LocalDateTime 형식, Nickname 추가

--- Problem-Service/src/main/java/com/gachonoj/problemservice/service/ExamService.java
+++ Problem-Service/src/main/java/com/gachonoj/problemservice/service/ExamService.java
@@ -14,6 +14,7 @@ import com.gachonoj.problemservice.feign.dto.response.SubmissionExamResultInfoRe
 import com.gachonoj.problemservice.repository.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.impl.cookie.DateUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
