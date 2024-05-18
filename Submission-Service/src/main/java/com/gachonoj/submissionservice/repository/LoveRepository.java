f830c94312da256abd9e6275394c03f88f78c009
NaMinhyeok <nmh9097@gmail.com>
2024-05-18 22:57:51 +0900

    [fix] #135 레포지토리 문법 수정
    레포지토리에서 실행안되고있어서 수정하였습니다.

--- Submission-Service/src/main/java/com/gachonoj/submissionservice/repository/LoveRepository.java
+++ Submission-Service/src/main/java/com/gachonoj/submissionservice/repository/LoveRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LoveRepository extends JpaRepository<Love,Long> {
     // 제출 id와 회원 id 존재하는지 조회
-    boolean existsBySubmissionIdAndMemberId(Long submissionId, Long memberId);
-    void deleteBySubmissionIdAndMemberId(Long submissionId, Long memberId);
+    boolean existsBySubmissionSubmissionIdAndMemberId(Long submissionId, Long memberId);
+    void deleteBySubmissionSubmissionIdAndMemberId(Long submissionId, Long memberId);
 }
