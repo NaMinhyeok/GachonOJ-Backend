aad827b268ee0aaa8e2da6e5ff796991bc89791f
NaMinhyeok <nmh9097@gmail.com>
2024-05-27 00:29:12 +0900

    [fix] #149 Feign Method 오류 수정

--- Problem-Service/src/main/java/com/gachonoj/problemservice/feign/client/AiServiceFeignClient.java
+++ Problem-Service/src/main/java/com/gachonoj/problemservice/feign/client/AiServiceFeignClient.java
@@ -1,9 +1,11 @@
 package com.gachonoj.problemservice.feign.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @FeignClient(name = "Ai-Service")
 public interface AiServiceFeignClient {
     // 문제에 대한 ai 분석 삭제
+    @DeleteMapping("/ai/problem")
     void deleteAiByProblemId(Long problemId);
 }
