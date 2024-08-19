b042a74e9c050bd0b3df00003cae35e07c6dfa4f
NaMinhyeok <nmh9097@gmail.com>
2024-08-19 15:27:37 +0900

    [refactor] #194 Query Counter 구현
    Query Counter 를 구현하여 각 메서드가 쿼리를 몇번 발생시키는지 확인 할 수 있도록 하였습니다

---
+++ Board-Service/src/main/java/com/gachonoj/boardservice/utils/querycounter/PerformanceAspect.java
@@ -0,0 +1,41 @@
+package com.gachonoj.boardservice.utils.querycounter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+
+@Aspect
+@Component
+@Slf4j
+public class PerformanceAspect {
+    private final ThreadLocal<ApiQueryCounter> queryCounter = new ThreadLocal<>();
+
+    @Pointcut("execution(* javax.sql.DataSource.getConnection(..))")
+    public void performancePointcut() {
+    }
+
+    @Around("performancePointcut()")
+    public Object start(ProceedingJoinPoint point) throws Throwable {
+        final Connection connection = (Connection) point.proceed();
+        queryCounter.set(new ApiQueryCounter());
+        final ApiQueryCounter counter = this.queryCounter.get();
+
+        final Connection proxyConnection = getProxyConnection(connection, counter);
+        queryCounter.remove();
+        return proxyConnection;
+    }
+
+    private Connection getProxyConnection(Connection connection, ApiQueryCounter counter) {
+        return (Connection) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{Connection.class},
+                new ConnectionHandler(connection, counter)
+        );
+    }
+}
